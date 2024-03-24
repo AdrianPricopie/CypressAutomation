bbeb7560b21b4eb8ff7ba0137d2ef3be26ce7cb9
AdrianPricopie <adi.pricopie@yahoo.com>
2024-03-24 23:34:55 +0200

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -2,10 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "xjd12e",
+  reporter: 'cypress-mochawesome-reporter', //for html reports
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      
+      require('cypress-mochawesome-reporter/plugin')(on); //for html esport
     },
     specPattern:'cypress/Integration/TestsOrangeHRM/*.js'
   },
